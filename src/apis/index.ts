d2f2a695ac45c6f4ba5a6837def4c3a66b5bca56
takecchi <takecchi@users.noreply.github.com>
2023-09-23 20:34:09 +0000

    0.0.2 - Updated

---
+++ src/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './PostsApi';
+export * from './SampleApi';
+export * from './TimelinesApi';
+export * from './UsersApi';
---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthResponse';
+export * from './CreatePost';
+export * from './LoginRequest';
+export * from './Post';
+export * from './PostResponse';
+export * from './PreUserRequest';
+export * from './TokenResponse';
+export * from './User';
+export * from './UserRequest';
+export * from './UserResponse';
+export * from './VerifyCodeRequest';
